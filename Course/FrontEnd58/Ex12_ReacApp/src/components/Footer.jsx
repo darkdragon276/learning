bd264ed950a95c4afc65e33b844c6737e59ebada
chuhainam <chuhai.nam1997@gmail.com>
2021-03-19 20:44:36 +0700

    add new Ex12

---
+++ Course/FrontEnd58/Ex12_ReacApp/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div className="footer-copy">
+                    <p><span>icon</span>Copyright.NewShop</p>
+                </div>
+                <div className="footer-card">
+                    <img src="./img/card.png" alt />
+                </div>
+            </footer>
+        )
+    }
+}
---
+++ Course/FrontEnd58/Ex12_ReacApp/src/components/Intro.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Intro extends Component {
+    render() {
+        return (
+            <section className="intro">
+                <div className="intro-content">
+                    <img src="./img/p1.jpg" alt />
+                </div>
+                <div className="intro-content">
+                    <img src="./img/p2.jpg" alt />
+                </div>
+                <div className="intro-content">
+                    <img src="./img/p3.jpg" alt />
+                </div>
+                <div className="intro-content">
+                    <img src="./img/p4.jpg" alt />
+                </div>
+            </section>
+
+        )
+    }
+}
